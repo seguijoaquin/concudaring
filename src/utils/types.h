5ae434a1ebc10ac8731b4b5daebcf82f2157dca3
seguijoaquin <segui.joaquin@gmail.com>
2017-04-08 14:33:09 -0300

    Agrego clase memoria compartida, constantes y tipos de datos

---
+++ src/utils/types.h
@@ -0,0 +1,15 @@
+#ifndef CONCUDARING_TYPES_H
+#define CONCUDARING_TYPES_H
+
+#include <iostream>
+
+struct card {
+  unsigned int number ;
+  char letter;
+};
+
+struct table {
+
+};
+
+#endif //CONCUDARING_TYPES_H
